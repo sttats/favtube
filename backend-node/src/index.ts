6a2855e6a32aad04d19086b993fd74e1e406154f
sttats <ajtgjm258456kam@gmail.com>
2026-03-03 22:59:30 +0900

    edit docker context 4

--- backend-node/src/index.ts
+++ backend-node/src/index.ts
@@ -1,7 +1,7 @@
 import express from 'express'; 
 import { fileURLToPath } from 'url';
 import path from 'path';
-import type { ApiHello } from '@favtube/types';
+import type { ApiHello } from './types.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
